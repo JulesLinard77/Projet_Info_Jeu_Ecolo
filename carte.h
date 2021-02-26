1c862306433be404c6a884ea952295729c908465
Maximilien Schollaert <maximilien.schollaert@ensiie.fr>
2021-02-26 18:45:31 +0100

    Tâche A.2: Ajout de l'implémentation des types "case" et "carte", et correction de l'implémentation de la fonction "finDuTour"

--- carte.h
+++ carte.h
@@ -3,13 +3,29 @@
 
 /*Définition du type carte*/
 
-/* Une case contient [A COMPLETER] */
-struct case;
+/* Identifiant des différents contenus des cases:
+- 0 si la case est vide
+- 1 si la case contient la porte transuniverselle
+- 2 si la case produit des ressources
+- 3 si la case contient un collecteur
+- 4 si la case contient un tapis roulant
+- 5 si la case contient une déchetterie
+- 6 si la case contient un centre de recyclage */
+
+
+/* Une case contient un entier représentant le contenu de la case (suivant les instructions ci dessus), et deux entiers positifs représentant les nombres de ressources et de déchets sur la case */
+struct case {
+   int contenu;
+   int nb_ressources;
+   int nb_dechets;
+}
 
 typedef struct case case;
 
 /* Une carte est un tableau de taille n*n dont les cases sont de type "case" */
-struct carte;
+struct carte {
+   case[][] carte;
+}
 
 typedef struct carte carte;
 
@@ -40,9 +56,9 @@ void recruterFISA(int *fisa, int nb_recrue);
    ensures incrémente */
 void changerRessourcesFISA(int *statut_fisa);
 
-/* requires rien
-   assigns *tour
+/* requires un pointeur vers la carte *map
+   assigns *map, *tour
    ensures incrémente le numéro de tour de 1 */
-void finDuTour();
+void finDuTour(carte *map);
 
 #endif
