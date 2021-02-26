985594f4b2d642a8133ed2f56ca1962dab9d6528
Jules LINARD <jules.linard@ensiie.fr>
2021-02-26 17:32:07 +0100

    Tâche A3 --> Personnel

---
+++ personnel.h
@@ -0,0 +1,14 @@
+#ifndef _PERSONNEL_H
+#define _PERSONNEL_H
+
+typedef struct personnel {
+    int numero;
+    char nom;
+    char prenom;
+    int cout_e;
+    int cout_ee;
+    int nombre_achete;
+} personnel;
+
+
+#endif
