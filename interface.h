c3b0d79296fe4b760e35f434fcd7c3b8669dac7c
thomas.idziejczak@ensiie.fr <thomas.idziejczak@ensiie.fr>
2021-03-05 15:00:07 +0100

    3ème version

--- interface.h
+++ interface.h
@@ -1,11 +1,15 @@
+#ifndef
+#DEFINE
+
 int DemandeTailleTbaleau(void);
 void AfficheCarte(carte Carte);
 int ActionJoueur(void);                 /* Prend comme valeur 1 pour passer au tour suivant, 2 pour ajouter une machine, 3 pour quitter.*/
 int Recruter(int* type_eleve, int* nombre_eleves, int verif);    /* Renvoit 0 s'il veut retourner au menu, 1 si c'est un fise et 2 pour un fisa */
 int NouvelleMachine(int* x, int* y, int verif);     /* Renvoit 0 s'il veut retourner au menu, sinon renvoit le numéro de la machine à ajouter, en fonction de la valeur de verif, renvoit le message d'erreur correpondant*/ /* Ancienne version: liste d'int de taille 3 (abscisse x, ordonéee y et machine à implémenter) 0 pour le collecteur de ressources ou de déchets, 1 pour le tapis roulant, 2 pour pour la Croix, 3 pour la Déchetterie, 4 pour le Centre de recyclage ex: Déchetterie en (x=17,y=11) alors liste=[16,10,3], message d'erreur si pas assez de ressource, ou s'il y a deja une machine, une porte ou si les coordonnées dépasse la taille de la carte. La liste d'int Personnel de longueur 24 permet de savoir si il y a des reductions ex: j'ai 1 Brunel et 3 Faye alors Personnel=[0,0,0,0,0,0,0,0,1,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0] */
 int ChangerMachine(int* x, int* y, int verif);      /* Renvoit 0 pour revenir au menu, 1 pour améliorer et 2 pour détruire si possible à chaque fois, en fonction de la valeur de verif, renvoit le message d'erreur correpondant */ /* liste d'int de taille 3 (abscisse x, ordonéee y et chose à lui faire) 0 pour détruire, 1 pour l'améliorer ex: Destruction de la machine en (x=18,y=8) alors liste=[17,7,0], afficher message d'erreur si l'on veut améliorer une machine non améliorable ou s'il n'y a pas de machine aux coordonnées rentrées. La liste d'int Personnel permet de savoir si il y a des reductions */
-int DemandePersonnel();                    /* Renvoit 0 pour revenir au menu, sinon renvoit le numéro du perosnnel. */ /* demande et renvoie le numéro correpondant au prof si les ressources le permettent, message d'erreur sinon. ex: avec 100 E et 100 DD si je veux faye et je tape 13 message d'erreur et on recommence à ActionJoueur, mais si je rentre 5 Pour Merabet alors izok et ca renvoie 5 (ou 4 pour faciliter les listes)*/
+int DemandePersonnel(int verif);                    /* Renvoit 0 pour revenir au menu, sinon renvoit le numéro du perosnnel. En fonction de la valeur de verif, renvoit le message d'erreur correpondant */ /* demande et renvoie le numéro correpondant au prof si les ressources le permettent, message d'erreur sinon. ex: avec 100 E et 100 DD si je veux faye et je tape 13 message d'erreur et on recommence à ActionJoueur, mais si je rentre 5 Pour Merabet alors izok et ca renvoie 5 (ou 4 pour faciliter les listes)*/
 
+#endif
 
 /* verif */
 
