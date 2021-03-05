1b3be41d206424fed97cf9f99fbd6c2b9b8c887f
Jules LINARD <jules.linard@ensiie.fr>
2021-03-05 15:02:21 +0100

    Tâche A.3 : Ajout des dernières fonctions de carte.h et modifications de personnel.h et de machine.h

--- carte.h
+++ carte.h
@@ -1,6 +1,9 @@
 #ifndef
 #DEFINE
 
+#include <stdlib.h>
+#include "machine.h"
+
 /*Définition du type carte*/
 
 /* Identifiant des différents contenus des cases:
@@ -10,9 +13,10 @@
 - 3 si la case contient une machine*/
 
 
-/* Une case contient un entier représentant le contenu de la case (suivant les instructions ci dessus), et deux entiers positifs représentant les nombres de ressources et de déchets sur la case */
+/* Une case contient un entier valant 1 si la porte si situe sur cette case, 0 sinon; un pointeur valant NULL si aucune machine n'est présente, une machine sinon ainsi que le nb de déchets et de ressources*/
 struct case {
-   int contenu;
+   int porte;
+   machine* machine;
    int nb_ressources;
    int nb_dechets;
 };
@@ -52,12 +56,34 @@ void recruterFISA(int *fisa, int nb_recrue);
 
 /* requires un pointeur *statut_fisa vers un entier valant 0 ou 1
    assigns *statut_fisa
-   ensures incrémente */
+   ensures change le type de ressources produits par les FISA */
 void changerRessourcesFISA(int *statut_fisa);
 
 /* requires un pointeur vers la carte *map, et un pointeur vers un entier *tour représentant le numéro de tour
    assigns *map, *tour
    ensures incrémente le numéro de tour de 1, et termine le tour suivant les règles définies dans la section 1.8 "Tour du jeu" du sujet */
 void finDuTour(carte *map, int *tour);
+
+/* requires un pointeur vers la carte map, l'abscisse et l'ordonée de la case où ajouter la machine, ainsi qu'un entier donnant l'utilisation de la machine
+   assigns *map
+   ensures ajoute une machine du type utilisation à la case (x,y) si on a le budget */
+void ajouterNouvelleMachine(carte *map,int x_case, int y_case,int utilisation);
+
+
+/*requires un pointeur vers la carte map, l'abscisse et l'ordonnée de la case où améliorer la machine
+   assigns *map
+   ensures améliore la machine se trouvant sur la case (x,y) si elle existe et qu'on a le budget */
+void ameliorerMachine(carte *map,int x_case,int y_case);
+
+/*requires un pointeur vers la carte map, l'abscisse et l'ordonnée de la case où améliorer la machine
+   assigns *map
+   ensures détruit la machine se trouvant sur la case (x,y) si elle existe et qu'on a le budget*/
+void detruireMachine(carte *map,int x_case,int y_case);
+
+/*requires le numero du membre du personnel que l'on souhaite acheter
+   assigns rien
+   ensures ajouter un membre du personnel au joueur
+*/
+void acheterMembrePersonnel(int numero);
 
 #endif
